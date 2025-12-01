3cbd45b33562f0b91fc37eeb0ab937abaeaa2412
Geoff Owen <geoff.owen@digital.mod.uk>
2025-12-01 12:20:25 +0000

    Updating pre-commit hook checks and PR checks

--- app/main.py
+++ app/main.py
@@ -2,9 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
     return "Hello from GitHub Container Registry & Helm chart! 🚀"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
